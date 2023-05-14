8fafa3feed3a6e947734f3de6e89035ac307f3cc
Tiendrebéogo <joycetiend@gmail.com>
2023-05-14 21:00:31 +0200

    firt_commit

---
+++ affichage.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <section class="s4">
+            <div class="container-fluid">
+                <div class="row row-cols-3 row4">
+                    <div class="col-md-4 col-sm-4  col1">
+                        <img class="img2" src="images/logo - Copie.png" alt="" height="200px"; width="200px";>
+                    </div>
+                    <div class="col-md-4 col-sm-4  col2" >
+                        <h1>Liste des étudiant</h1>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col3" >
+                        <h3>Burkina-Faso</h3>
+                        <p>Unité-Progrès-Justice</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </header> <br> <br>
+    <section class="s3">
+        <div>id</div>
+        <div>nom</div>
+        <div>prénom(s)</div>
+        <div>Date de naissance</div>
+        <div>Genre</div>
+        <div>Date d'inscription</div>
+        <div>I.C</div>
+</section>
+<section class="s3">
+    <div>1</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section>
+<section class="s3">
+    <div>2</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section>
+<section class="s3">
+    <div>3</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section>
+<section class="s3">
+    <div>4</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section>
+<section class="s3">
+    <div>5</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section>
+<section class="s3">
+    <div>6</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section>
+<section class="s3">
+    <div>7</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section>
+<section class="s3">
+    <div>8</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section>
+<section class="s3">
+    <div>9</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section>
+<section class="s3">
+    <div>10</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</section> <br> 
+<footer>
+    <P>Tel: +226 25-30-70-64 <br>
+       Fax: +226 25-30-72-40
+    </P>
+    <hr> <br><br><br>
+    <a href="https://www.facebook.com/profile.php?id=100064838614968">Ufr/sds de l'Université Ouaga I professeur Joseph Ki-Zerbo</a>
+</footer>
+</body>
+</html>
